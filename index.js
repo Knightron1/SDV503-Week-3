e7e6667b802a5e1c48f2ea6a87995c67b2c44e93
Knightron1 <61396001+Knightron1@users.noreply.github.com>
2020-03-09 13:30:55 +1300

    maths

---
+++ index.js
@@ -0,0 +1,14 @@
+let a = 2, b = 3, c = 4, d = 5;
+
+var sum = a + b;
+console.log(a + " + " + b + " = " + sum);
+var sub = b - a;
+console.log(b + " - " + a + " = " + sub);
+var div = c / a;
+console.log(c + " / " + a +  " = " + div);
+var perc = d % a;
+console.log(d + " % " +a+ " = " + perc);
+var mult = a * b;
+console.log(a +" * " +b +" = " + mult);
+var expo = a ** b;
+console.log(a + " to the power of " + b + " = " + expo);
